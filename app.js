cf693b53d089920c00eaae965eb1866872eed855
andrezsilva0366 <andrez.silva.0366@gmail.com>
2025-10-23 22:50:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // --- CONFIGURAÇÃO ---
     // Cole seu ID do Formspree aqui
-    const FORMSPREE_ID = "https://formspree.io/f/xovkgnnd"; 
+    const FORMSPREE_ID = "xovkgnnd"; 
     // --------------------
 
     const infoPedido = document.getElementById("info-pedido");
